bc43e5e97af404d1504ab49652d6fe944227719c
Wes <ParkGalaxy@live.com>
2022-09-01 16:39:26 -0400

    Update factories

--- Airdrop/AirdropFactories/Holdings/LundisHoldingsFactory.cs
+++ Airdrop/AirdropFactories/Holdings/LundisHoldingsFactory.cs
@@ -33,6 +33,7 @@ namespace Airdrop.AirdropFactories.Holdings
             this.RevokedAddresses = new string[]
             {
                 "LUNDIW5HP5KWUG6JTXHMDB5XPNXQ43OIBXET3RTBXNVJISQUTBKWONTGNI",
+                "UB6E3ACF7MHSAY5LRRNUHMW55IAQGGWOVADTVDNMF7JL7CA25GO7V4F2MI"
             };
             this.AssetValue = 10;
         }
