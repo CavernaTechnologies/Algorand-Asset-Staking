8f426ea48dab2fcb7d94cf00dc2f66ca57c84887
Wes <ParkGalaxy@live.com>
2022-10-05 16:21:20 -0400

    Update BuzzyBees Add Yarn

--- Airdrop/AirdropFactories/Holdings/BuzzyBeesHoldingsFactory.cs
+++ Airdrop/AirdropFactories/Holdings/BuzzyBeesHoldingsFactory.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Algorand.V2.Algod.Model;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using Utils.Algod;
 using Utils.Indexer;
 
@@ -16,19 +19,50 @@ namespace Airdrop.AirdropFactories.Holdings
             this.Decimals = 0;
             this.CreatorAddresses = new string[] {
                 "BEEE3WGLXN6QD62D3LVB67DF5LESMIS6FVD4QTMOV3325OM24CDQKBWI6U",
+                "HKBG45ZMBYAJPDRI4GZJI4HWB7ZEKPHH6EETDQ563KTMNUTOGTR7CZZPEI"
             };
             this.RevokedAssets = new ulong[]
             {
             };
             this.RevokedAddresses = new string[]
             {
-                "HNYWPQPSKMYGVOY2HTZQGMD5JUANMVGIOFPALVZCC6YGFHOWAYWQCC2AJE"
+                "HNYWPQPSKMYGVOY2HTZQGMD5JUANMVGIOFPALVZCC6YGFHOWAYWQCC2AJE",
+                "HKBG45ZMBYAJPDRI4GZJI4HWB7ZEKPHH6EETDQ563KTMNUTOGTR7CZZPEI"
             };
             this.AssetValue = 10;
             this.SearchAlandia = true;
             this.SearchRand = true;
             this.SearchAlgox = true;
-            this.AlgoxCollectionNames = new string[] { "buzzy-bees" };
+            this.AlgoxCollectionNames = new string[] { "buzzy-bees", "buzzy-bees-hive-keepers" };
+        }
+
+        public async override Task<IDictionary<ulong, ulong>> FetchAssetValues()
+        {
+            Dictionary<ulong, ulong> assetValues = new Dictionary<ulong, ulong>();
+
+            foreach (string creatorAddress in this.CreatorAddresses.Take(1))
+            {
+                Account account = await this.AlgodUtils.GetAccount(creatorAddress);
+                var assets = account.CreatedAssets;
+
+                foreach (var asset in assets)
+                {
+                    assetValues.Add(asset.Index, this.AssetValue);
+                }
+            }
+
+            foreach (string creatorAddress in this.CreatorAddresses.Skip(1).Take(1))
+            {
+                Account account = await this.AlgodUtils.GetAccount(creatorAddress);
+                var assets = account.CreatedAssets;
+
+                foreach (var asset in assets)
+                {
+                    assetValues.Add(asset.Index, 2);
+                }
+            }
+
+            return assetValues;
         }
     }
 }
---
+++ Airdrop/AirdropFactories/Holdings/YarnHoldingsFactory.cs
@@ -0,0 +1,93 @@
+﻿using Algorand.V2.Algod.Model;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Algod;
+using Utils.Indexer;
+
+namespace Airdrop.AirdropFactories.Holdings
+{
+    public class YarnHoldingsFactory : ExchangeHoldingsAirdropFactory
+    {
+        public YarnHoldingsFactory(IIndexerUtils indexerUtils, IAlgodUtils algodUtils, IConfiguration config, IHttpClientFactory httpClientFactory) : base(indexerUtils, algodUtils, config, httpClientFactory.CreateClient())
+        {
+            this.DropAssetId = 878951062;
+            this.Decimals = 0;
+            this.CreatorAddresses = new string[]
+            {
+                "45LDVA6A44QD2PNWNAPGGDQESXNOY36HJC6UZXZNMIAYLXUYD4DGRAMNNA",
+                "FROGJWNVWICMFTAGNCLGAI5UNXJEEFCUDNPBD2U6VWTATXANHXB6BHRW2M",
+                "FROGOHZ3D5GHBKPDDQWKT5RWABZGY3VD6D3UNG7YLTABKDDLJJD4E257HA",
+            };
+            this.SearchRand = true;
+            this.SearchAlgox = true;
+            this.SearchAlgox = true;
+            this.AlgoxCollectionNames = new string[]
+            {
+                "knit-heads",
+                "lil-knits"
+            };
+        }
+
+        public async override Task<IDictionary<ulong, ulong>> FetchAssetValues()
+        {
+            Dictionary<ulong, ulong> assetValues = new Dictionary<ulong, ulong>();
+
+            foreach (string creatorAddress in this.CreatorAddresses.Take(1))
+            {
+                Account account = await this.AlgodUtils.GetAccount(creatorAddress);
+                var assets = account.CreatedAssets;
+
+                foreach (var asset in assets)
+                {
+                    if (asset.Params.UnitName.StartsWith("KH"))
+                    {
+                        assetValues.Add(asset.Index, 300);
+                    }
+                }
+            }
+
+            foreach (string creatorAddress in this.CreatorAddresses.Take(1))
+            {
+                Account account = await this.AlgodUtils.GetAccount(creatorAddress);
+                var assets = account.CreatedAssets;
+
+                foreach (var asset in assets)
+                {
+                    if (asset.Params.UnitName.StartsWith("frogBOT"))
+                    {
+                        assetValues.Add(asset.Index, 2);
+                    }
+                }
+            }
+
+            foreach (string creatorAddress in this.CreatorAddresses.Skip(1).Take(1))
+            {
+                Account account = await this.AlgodUtils.GetAccount(creatorAddress);
+                var assets = account.CreatedAssets;
+
+                foreach (var asset in assets)
+                {
+                    assetValues.Add(asset.Index, 40);
+                }
+            }
+
+            foreach (string creatorAddress in this.CreatorAddresses.Skip(2).Take(1))
+            {
+                Account account = await this.AlgodUtils.GetAccount(creatorAddress);
+                var assets = account.CreatedAssets;
+
+                foreach (var asset in assets)
+                {
+                    assetValues.Add(asset.Index, 333);
+                }
+            }
+
+            return assetValues;
+        }
+    }
+}
