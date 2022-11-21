28ceed66bb9194759ad2c2887134e51c55455d9a
Wes <ParkGalaxy@live.com>
2022-11-20 20:28:00 -0500

    Update high hogs

--- Airdrop/AirdropFactories/Holdings/HighHogHoldingsFactory.cs
+++ Airdrop/AirdropFactories/Holdings/HighHogHoldingsFactory.cs
@@ -12,11 +12,17 @@ namespace Airdrop.AirdropFactories.Holdings
     {
         public HighHogHoldingsFactory(IIndexerUtils indexerUtils, IAlgodUtils algodUtils, IConfiguration config, IHttpClientFactory httpClientFactory) : base(indexerUtils, algodUtils, config, httpClientFactory.CreateClient())
         {
-            this.DropAssetId = 455356741;
+            this.DropAssetId = 844772414;
             this.Decimals = 0;
-            this.CreatorAddresses = new string[] { "YZEDY575NGDQPBQT3FD5C6S56DFNNICIGN32X2GI6AC63MVOPZMWVOZY7Y" };
+            this.CreatorAddresses = new string[] { "25YRTO46IOKSN66T3GTSFUUYXS7HDCFA4DRKUYZ6RDNKYXUDZ5GYARZ4QE" };
             this.AssetValue = 5000;
             this.SearchRand = true;
+            this.SearchAlandia = true;
+            this.SearchAlgox= true;
+            this.AlgoxCollectionNames = new string[]
+            {
+                "high-hogs-reborn",
+            };
         }
     }
 }
