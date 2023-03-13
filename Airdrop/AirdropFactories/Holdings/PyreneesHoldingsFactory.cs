a274c7ea1e951a82987a0b1a101fd8f9bf85a236
Wes <millerwes1998@gmail.com>
2023-03-13 12:58:58 -0400

    Update pyrenees

--- Airdrop/AirdropFactories/Holdings/PyreneesHoldingsFactory.cs
+++ Airdrop/AirdropFactories/Holdings/PyreneesHoldingsFactory.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Algorand.V2.Algod.Model;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using Utils.Algod;
+using Utils.Cosmos;
 using Utils.Indexer;
 
 namespace Airdrop.AirdropFactories.Holdings
@@ -14,11 +18,42 @@ namespace Airdrop.AirdropFactories.Holdings
         {
             this.DropAssetId = 765722712;
             this.Decimals = 0;
-            this.CreatorAddresses = new string[] { "IEQGNR7DPQ26ZY7A22VHRTLHDL2PZ3XD5AUMMEEPYB6DUL6FSRN2NUUWPE" };
-            this.RevokedAddresses = new string[] { "IEQGNR7DPQ26ZY7A22VHRTLHDL2PZ3XD5AUMMEEPYB6DUL6FSRN2NUUWPE" };
+            this.CreatorAddresses = new string[] { "IEQGNR7DPQ26ZY7A22VHRTLHDL2PZ3XD5AUMMEEPYB6DUL6FSRN2NUUWPE", "PUPSJCLH66O6L6BNBT3OTVECXH3QXRJ5F2RKDIC4XQBMHYH2WGQUB6MKUY" };
+            this.RevokedAddresses = new string[] { "IEQGNR7DPQ26ZY7A22VHRTLHDL2PZ3XD5AUMMEEPYB6DUL6FSRN2NUUWPE", "PUPSJCLH66O6L6BNBT3OTVECXH3QXRJ5F2RKDIC4XQBMHYH2WGQUB6MKUY" };
             this.RevokedAssets = new ulong[] { };
             this.AssetValue = 200;
             this.SearchRand = true;
+        }
+
+        public async override Task<IDictionary<ulong, ulong>> FetchAssetValues()
+        {
+            Dictionary<ulong, ulong> assetValues = new Dictionary<ulong, ulong>();
+
+            ulong pixValue = 200;
+
+            foreach (string creatorAddress in this.CreatorAddresses.Take(1))
+            {
+                Account account = await this.AlgodUtils.GetAccount(creatorAddress);
+                var assets = account.CreatedAssets;
+                foreach (var asset in assets)
+                {
+                    assetValues.Add(asset.Index, pixValue);
+                }
+            }
+
+            ulong wuValue = 20;
+
+            foreach (string creatorAddress in this.CreatorAddresses.Skip(1).Take(1))
+            {
+                Account account = await this.AlgodUtils.GetAccount(creatorAddress);
+                var assets = account.CreatedAssets;
+                foreach (var asset in assets)
+                {
+                    assetValues.Add(asset.Index, wuValue);
+                }
+            }
+
+            return assetValues;
         }
     }
 }
